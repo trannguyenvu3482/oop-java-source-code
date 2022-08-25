f58ad8aa4b0651b500d14d09fabee1727147b366
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-08-25 09:56:59 +0700

    Edited Module02

--- Module02/src/bai11/SoTietKiem.java
+++ Module02/src/bai11/SoTietKiem.java
@@ -1,6 +1,7 @@
 package bai11;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class SoTietKiem {
     private String maSo;
@@ -18,5 +19,66 @@ public class SoTietKiem {
         this.soTienGui = soTienGui;
         this.kyHan = kyHan;
         this.laiSuat = laiSuat;
+    }
+
+    public int tinhSoThangGuiThuc() {
+        Period age = Period.between(ngayMoSo, LocalDate.now());
+        int yearsBetween = age.getYears();
+        int monthsBetween = age.getMonths();
+        int monthsSent = yearsBetween * 12 + monthsBetween;
+        if (monthsSent % kyHan == 0) {
+            return monthsSent;
+        } else {
+            return monthsSent / kyHan * kyHan;
+        }
+    }
+
+    public double tinhTienLai() {
+        return soTienGui * Math.pow(1 + laiSuat, tinhSoThangGuiThuc());
+    }
+
+    public String getMaSo() {
+        return maSo;
+    }
+
+    public void setMaSo(String maSo) {
+        this.maSo = maSo;
+    }
+
+    public LocalDate getNgayMoSo() {
+        return ngayMoSo;
+    }
+
+    public void setNgayMoSo(LocalDate ngayMoSo) {
+        this.ngayMoSo = ngayMoSo;
+    }
+
+    public double getSoTienGui() {
+        return soTienGui;
+    }
+
+    public void setSoTienGui(double soTienGui) {
+        this.soTienGui = soTienGui;
+    }
+
+    public int getKyHan() {
+        return kyHan;
+    }
+
+    public void setKyHan(int kyHan) {
+        this.kyHan = kyHan;
+    }
+
+    public float getLaiSuat() {
+        return laiSuat;
+    }
+
+    public void setLaiSuat(float laiSuat) {
+        this.laiSuat = laiSuat;
+    }
+
+    @Override
+    public String toString() {
+        return "STK " + maSo + "[" + ngayMoSo + "-" + LocalDate.now() + "]KH " + kyHan + " thang"
     }
 }
