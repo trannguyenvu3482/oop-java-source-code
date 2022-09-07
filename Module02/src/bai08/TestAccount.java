e2328c258eaf5188dc16bea0eec8d292c15bfb7b
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-09-07 11:38:18 +0700

    fixed head

--- Module02/src/bai08/TestAccount.java
+++ Module02/src/bai08/TestAccount.java
@@ -1,4 +1,4 @@
-<<<<<<<HEAD package bai08;
+package bai08;
 
 public class TestAccount {
 	public static void main(String[] args) {
@@ -31,40 +31,4 @@ public class TestAccount {
 		System.out.println(acc1);
 		System.out.println(acc2);
 	}
-}=======
-package bai08;
-
-public class TestAccount {
-	public static void main(String[] args) {
-        Account acc1 = new Account("Tran Van A", 72354, 102.56);
-        Account acc2 = new Account("Nguyen Van B", 69713, 40.00);
-        Account acc3 = new Account("Tran Nguyen Vu", 93757, 759.32);
-
-        acc1.deposit(25.85);
-        acc2.deposit(500.00);
-        acc2.withdraw(430.75, 1.50);
-
-        acc3.addInterest();
-
-        System.out.println(String.format("%-20s %-20s %-20s", "Name", "Account Number", "Balance"));
-        System.out.println(acc1);
-        System.out.println(acc2);
-        System.out.println(acc3);
-
-        System.out.println("\nDang chuyen 100.00 tu tai khoan " + acc1.getAccountNumber() + " sang tai khoan "
-                + acc2.getAccountNumber());
-
-        if (acc1.transfer(acc2, 100.00)) {
-            System.out.println("Chuyen thanh cong\n");
-        } else {
-            System.out.println("Chuyen that bai\n");
-        }
-
-        System.out.println(String.format("%-20s %-20s %-20s", "Name", "Account Number", "Balance"));
-        System.out.println(acc1);
-        System.out.println(acc2);
-    }
-}>>>>>>>branch'master'
-
-of https:
-// github.com/trannguyenvu3482/oop-java-source-code.git
+}
--- Module02/src/bai15/TestDiemHocVien.java
+++ Module02/src/bai15/TestDiemHocVien.java
@@ -1,37 +1,17 @@
-<<<<<<< HEAD
 package bai15;
 
 public class TestDiemHocVien {
-    public static void main(String[] args) {
-        DiemHocVien dhv = new DiemHocVien();
+	public static void main(String[] args) {
+		DiemHocVien dhv = new DiemHocVien();
 
-        dhv.themDiemHocVien(new HocVien(1, 2, 3, 4, 5));
-        dhv.themDiemHocVien(new HocVien(2, 3, 8, 8, 10));
-        dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
-        dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
-        dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
-        dhv.themDiemHocVien(new HocVien(10, 10, 10, 10, 10));
-        dhv.countSoLuong();
+		dhv.themDiemHocVien(new HocVien(1, 2, 3, 4, 5));
+		dhv.themDiemHocVien(new HocVien(2, 3, 8, 8, 10));
+		dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
+		dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
+		dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
+		dhv.themDiemHocVien(new HocVien(10, 10, 10, 10, 10));
+		dhv.countSoLuong();
 
-        System.out.println(dhv);
-    }
+		System.out.println(dhv);
+	}
 }
-=======
-package bai15;
-
-public class TestDiemHocVien {
-    public static void main(String[] args) {
-        DiemHocVien dhv = new DiemHocVien();
-
-        dhv.themDiemHocVien(new HocVien(1, 2, 3, 4, 5));
-        dhv.themDiemHocVien(new HocVien(2, 3, 8, 8, 10));
-        dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
-        dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
-        dhv.themDiemHocVien(new HocVien(6, 7, 6, 5, 5));
-        dhv.themDiemHocVien(new HocVien(10, 10, 10, 10, 10));
-        dhv.countSoLuong();
-
-        System.out.println(dhv);
-    }
-}
->>>>>>> branch 'master' of https://github.com/trannguyenvu3482/oop-java-source-code.git
