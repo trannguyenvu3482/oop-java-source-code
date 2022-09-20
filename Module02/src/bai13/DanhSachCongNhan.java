2baf1121993f47751ebbe60e87ac6b98fc91270a
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-09-20 22:48:19 +0700

    fix CongNhan of Module2

--- Module02/src/bai13/DanhSachCongNhan.java
+++ Module02/src/bai13/DanhSachCongNhan.java
@@ -3,97 +3,134 @@ package bai13;
 import java.util.Scanner;
 
 public class DanhSachCongNhan {
-    private CongNhan[] DSCN;
-    private int n;
-    private int i = 0;
-    private Scanner sc;
+	private CongNhan[] DSCN;
+	private int n;
+	private int i = 0;
+	private Scanner sc;
 
-    // Constructor
-    public DanhSachCongNhan() {
-        sc = new Scanner(System.in);
-        do {
-            System.out.println("Nhap so luong cong nhan: ");
-            n = sc.nextInt();
+	// Constructor
+	public DanhSachCongNhan() {
+		sc = new Scanner(System.in);
+		do {
+			System.out.println("Nhap so luong cong nhan: ");
+			n = sc.nextInt();
 
-            if (n < 1) {
-                System.out.println("So luong phai lon hon 0!");
-            }
-        } while (n < 1);
-        DSCN = new CongNhan[n];
-    }
+			if (n < 1) {
+				System.out.println("So luong phai lon hon 0!");
+			}
+		} while (n < 1);
+		DSCN = new CongNhan[n];
+	}
 
-    // Methods
-    // Add CongNhan to List
-    public void addCongNhan() {
-        System.out.println("Nhap thong tin cong nhan moi: ");
-        System.out.println("Nhap ma cong nhan: ");
-        int maCN = sc.nextInt();
-        System.out.println("Nhap ho cong nhan: ");
-        String mHo = sc.next();
-        System.out.println("Nhap ten cong nhan: ");
-        String mTen = sc.next();
-        System.out.println("Nhap so san pham: ");
-        int mSoSP = sc.nextInt();
+	// Methods
+	// Add CongNhan to List
+	public void addCongNhan() {
+		System.out.println("Nhap thong tin cong nhan moi: ");
+		System.out.println("Nhap ma cong nhan: ");
+		int maCN = sc.nextInt();
+		System.out.println("Nhap ho cong nhan: ");
+		String mHo = sc.next();
+		System.out.println("Nhap ten cong nhan: ");
+		String mTen = sc.next();
+		System.out.println("Nhap so san pham: ");
+		int mSoSP = sc.nextInt();
 
-        // Add new CongNhan to array if array is not full and maCN is unique
-        if (this.i < DSCN.length && checkMaCN(maCN) == false) {
-            DSCN[this.i] = new CongNhan(maCN, mHo, mTen, mSoSP);
-            this.i = this.i + 1;
-        } else {
-            System.out.println("Khong the them cong nhan vao danh sach");
-        }
-    }
+		// Add new CongNhan to array if array is not full and maCN is unique
+		if (this.i < DSCN.length && !checkMaCN(maCN)) {
+			DSCN[this.i] = new CongNhan(maCN, mHo, mTen, mSoSP);
+			this.i = this.i + 1;
+		} else {
+			System.out.println("Khong the them cong nhan vao danh sach");
+		}
+	}
 
-    // Check if maCN is already in the list
-    public boolean checkMaCN(int maCN) {
-        if (this.i == 0)
-            return false;
+	// Delete CongNhan by MaCN
+	public boolean deleteCongNhanByMaCN(int maCN) {
+		int res = findCongNhanByMaCN(maCN);
 
-        for (int j = 0; j < this.i; j++) {
-            if (DSCN[j].getMaCN() == maCN) {
-                return true;
-            }
-        }
-        return false;
-    }
+		if (res != -1) {
+			if (this.i == 1) {
+				System.out.println("Da xoa CN " + DSCN[res].getMaCN());
+				this.i = this.i - 1;
+				DSCN = new CongNhan[n];
+				return true;
+			} else {
+				System.out.println("Da xoa CN " + DSCN[res].getMaCN());
+				for (int j = res; j < this.i - 1; j++) {
+					DSCN[j] = DSCN[j + 1];
+				}
+				this.i = this.i - 1;
+				return true;
+			}
+		} else {
+			System.out.println("Khong ton tai CN co maCN day!");
+			return false;
+		}
+	}
 
-    // Output all CongNhan in the list
-    public void outputDSCN() {
-        System.out.println("Danh sach cong nhan: ");
-        System.out.println(
-                String.format("%-20s %-20s %-20s %-20s", "Ma cong nhan", "Ho va ten CN", "So san pham", "Tien luong"));
-        for (int j = 0; j < this.i; j++) {
-            System.out.println(DSCN[j].toString());
-        }
-    }
+	// Find a CongNhan by maCN, return index
+	public int findCongNhanByMaCN(int maCN) {
+		if (this.i == 0)
+			return -1;
 
-    // Output number of CongNhan in the list
-    public void outputSoLuong() {
-        System.out.println("So luong cong nhan: " + this.i);
-    }
+		for (int j = 0; j < this.i; j++) {
+			if (DSCN[j].getMaCN() == maCN) {
+				return j;
+			}
+		}
+		return -1;
+	}
 
-    // Output all CongNhan have mSoSP > 200
-    public void outputDSCN_mSoSP() {
-        System.out.println("Danh sach cong nhan co so san pham lon hon 200: ");
-        System.out.println(
-                String.format("%-20s %-20s %-20s %-20s", "Ma cong nhan", "Ho va ten CN", "So san pham", "Tien luong"));
-        for (int j = 0; j < this.i; j++) {
-            if (DSCN[j].getmSoSP() > 200) {
-                System.out.println(DSCN[j].toString());
-            }
-        }
-    }
+	// Check if maCN is already in the list, return boolean
+	public boolean checkMaCN(int maCN) {
+		if (this.i == 0)
+			return false;
 
-    // Sort all CongNhan by mSoSP
-    public void sortDSCN() {
-        for (int j = 0; j < this.i; j++) {
-            for (int k = j + 1; k < this.i; k++) {
-                if (DSCN[j].getmSoSP() < DSCN[k].getmSoSP()) {
-                    CongNhan temp = DSCN[j];
-                    DSCN[j] = DSCN[k];
-                    DSCN[k] = temp;
-                }
-            }
-        }
-    }
+		for (int j = 0; j < this.i; j++) {
+			if (DSCN[j].getMaCN() == maCN) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Output all CongNhan in the list
+	public void outputDSCN() {
+		System.out.println("Danh sach cong nhan: ");
+		System.out.println(
+				String.format("%-20s %-20s %-20s %-20s", "Ma cong nhan", "Ho va ten CN", "So san pham", "Tien luong"));
+		for (int j = 0; j < this.i; j++) {
+			System.out.println(DSCN[j].toString());
+		}
+	}
+
+	// Output number of CongNhan in the list
+	public void outputSoLuong() {
+		System.out.println("So luong cong nhan: " + this.i);
+	}
+
+	// Output all CongNhan have mSoSP > 200
+	public void outputDSCN_mSoSP() {
+		System.out.println("Danh sach cong nhan co so san pham lon hon 200: ");
+		System.out.println(
+				String.format("%-20s %-20s %-20s %-20s", "Ma cong nhan", "Ho va ten CN", "So san pham", "Tien luong"));
+		for (int j = 0; j < this.i; j++) {
+			if (DSCN[j].getmSoSP() > 200) {
+				System.out.println(DSCN[j].toString());
+			}
+		}
+	}
+
+	// Sort all CongNhan by mSoSP
+	public void sortDSCN() {
+		for (int j = 0; j < this.i; j++) {
+			for (int k = j + 1; k < this.i; k++) {
+				if (DSCN[j].getmSoSP() < DSCN[k].getmSoSP()) {
+					CongNhan temp = DSCN[j];
+					DSCN[j] = DSCN[k];
+					DSCN[k] = temp;
+				}
+			}
+		}
+	}
 }
--- Module02/src/bai13/TestCongNhan.java
+++ Module02/src/bai13/TestCongNhan.java
@@ -1,81 +1,59 @@
 package bai13;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class TestCongNhan {
-    // Clear console
-    public static void clearConsole() {
-        try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		int choice = -1;
+		boolean exit = false;
+		Scanner sc = new Scanner(System.in);
+		DanhSachCongNhan ds = new DanhSachCongNhan();
 
-    // Pause console
-    public static void pauseConsole() {
-        System.out.println("\nBam Enter de tiep tuc...");
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-    }
+		// Menu
+		do {
+			do {
+				System.out.println("1. Them cong nhan vao danh sach");
+				System.out.println("2. Xem danh sach cong nhan");
+				System.out.println("3. Xem so luong tat ca nhan vien co trong danh sach");
+				System.out.println("4. Xoa cong nhan theo ma cong nhan");
+				System.out.println("5. Xuat thong tin cac nhan vien lam tren 200 san pham");
+				System.out.println("6. Sap xep cong nhan theo so san pham giam dan");
+				System.out.println("7. Thoat");
+				System.out.println("Nhap lua chon: ");
+				choice = sc.nextInt();
 
-    public static void main(String[] args) {
-        int choice = -1;
-        boolean exit = false;
-        Scanner sc = new Scanner(System.in);
-        DanhSachCongNhan ds = new DanhSachCongNhan();
+				if (choice < 1 || choice > 7) {
+					System.out.println("Lua chon khong hop le. Nhap lai: ");
+				}
+			} while (choice < 1 || choice > 7);
 
-        // Menu
-        do {
-            do {
-                clearConsole();
-                System.out.println("1. Them cong nhan vao danh sach");
-                System.out.println("2. Xem danh sach cong nhan");
-                System.out.println("3. Tinh so luong tat ca nhan vien co trong danh sach");
-                System.out.println("4. Xuat thong tin cac nhan vien lam tren 200 san pham");
-                System.out.println("5. Sap xep cong nhan theo so san pham giam dan");
-                System.out.println("6. Thoat");
-                System.out.println("Nhap lua chon: ");
-                choice = sc.nextInt();
-
-                if (choice < 1 || choice > 6) {
-                    System.out.println("Lua chon khong hop le. Nhap lai: ");
-                }
-            } while (choice < 1 || choice > 6);
-
-            switch (choice) {
-                case 1:
-                    clearConsole();
-                    ds.addCongNhan();
-                    pauseConsole();
-                    break;
-                case 2:
-                    clearConsole();
-                    ds.outputDSCN();
-                    pauseConsole();
-                    break;
-                case 3:
-                    clearConsole();
-                    ds.outputSoLuong();
-                    pauseConsole();
-                    break;
-                case 4:
-                    clearConsole();
-                    ds.outputDSCN_mSoSP();
-                    pauseConsole();
-                    break;
-                case 5:
-                    clearConsole();
-                    ds.sortDSCN();
-                    System.out.println("Da sap xep!");
-                    pauseConsole();
-                    break;
-                case 6:
-                    System.out.println("Cam on ban da su dung chuong trinh");
-                    exit = true;
-                    break;
-            }
-        } while (!exit);
-    }
+			switch (choice) {
+			case 1:
+				ds.addCongNhan();
+				break;
+			case 2:
+				ds.outputDSCN();
+				break;
+			case 3:
+				ds.outputSoLuong();
+				break;
+			case 4:
+				System.out.println("Nhap vao ma CN can xoa: ");
+				int delMaCN = sc.nextInt();
+				ds.deleteCongNhanByMaCN(delMaCN);
+				break;
+			case 5:
+				ds.outputDSCN_mSoSP();
+				break;
+			case 6:
+				ds.sortDSCN();
+				System.out.println("Da sap xep!");
+				break;
+			case 7:
+				System.out.println("Cam on ban da su dung chuong trinh");
+				exit = true;
+				break;
+			}
+		} while (!exit);
+	}
 }
